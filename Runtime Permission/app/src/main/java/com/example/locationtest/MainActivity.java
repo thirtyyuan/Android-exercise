8654969e27b5088bd28622ea497ba9da27d588b1
thirtyyuan <thirtyyuan@gmail.com>
2016-07-31 22:29:00 +0800

    change some detail

--- Runtime Permission/app/src/main/java/com/example/locationtest/MainActivity.java
+++ Runtime Permission/app/src/main/java/com/example/locationtest/MainActivity.java
@@ -3,17 +3,29 @@ package com.example.locationtest;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 /*
@@ -27,7 +39,8 @@ public class MainActivity extends Activity {
     private String provider;
     private TextView positionTextView;
     private LocationManager locationManager;
-    public static final int LOCATION_ASK_CODE = 7 ;
+    public static final int SHOW_LOCATION = 0;
+    public static final int MULTIPLE_PERMISSION_ASK_CODE = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,28 +59,50 @@ public class MainActivity extends Activity {
 
     public void checkPermission(){
         //判断当前Activity是否已经获得了该权限
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationProvider();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestMultiplePermissions();
         } else {
-            //请求用户授予权限
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_ASK_CODE);
+            locationProvider();
         }
+    }
+
+    private void requestMultiplePermissions() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA};
+        ActivityCompat.requestPermissions(this, permissions, MULTIPLE_PERMISSION_ASK_CODE);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case LOCATION_ASK_CODE: {
-                // 如果请求被拒绝，那么通常grantResults数组为空
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //申请成功，进行相应操作
-                    Toast.makeText(this,"Thank you for your grant! ",Toast.LENGTH_SHORT).show();
-                    locationProvider();
-                } else {
+            case MULTIPLE_PERMISSION_ASK_CODE: {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     //申请失败，可以继续向用户解释
-                    Toast.makeText(this,"OH...Permission has been Denied.",Toast.LENGTH_SHORT).show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Help")
+                            .setMessage("Need some permission to run this app.")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    checkPermission();
+                                }
+                            })
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            })
+                            .show();
                 }
-                return;
+//                // 如果请求被拒绝，那么通常grantResults数组为空
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    //申请成功，进行相应操作
+//                    Toast.makeText(this,"Thank you for your grant 1! ",Toast.LENGTH_SHORT).show();
+//                    locationProvider();
+//                } else {
+//
+//                }
             }
         }
     }
@@ -127,8 +162,57 @@ public class MainActivity extends Activity {
     }
 
     //可能将来其他地方会直接调用展示而不是再次定位，写在此处提高复用率，降低代码耦合
-    private void showLocation (Location location) {
-        String currentPosition = "latitude is " + location.getLatitude() + "\n longitude is " + location.getLongitude();
-        positionTextView.setText(currentPosition);
+    private void showLocation (final Location location) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    StringBuilder urlStr = new StringBuilder();
+                    urlStr.append("http://maps.googleapis.com/maps/api/geocode/json?latlng=");
+                    urlStr.append(location.getLatitude()).append(",");
+                    urlStr.append(location.getLongitude());
+                    urlStr.append("&sensor=false");
+                    URL url = new URL(urlStr.toString());
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    JSONObject jsonObject = new JSONObject(String.valueOf(response));
+                    //获取 result 节点下的位置信息
+                    JSONArray resultArray = jsonObject.getJSONArray("results");
+                    if (resultArray.length() > 0) {
+                        JSONObject subObject = resultArray.getJSONObject(0);
+                        //取出格式化后的位置信息
+                        String  address = subObject.getString("formatted_address");
+                        Message message = new Message();
+                        message.what = SHOW_LOCATION;
+                        message.obj = address;
+                        handler.sendMessage(message);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_LOCATION:
+                    String currentPosition = (String) msg.obj;
+                    positionTextView.setText(currentPosition);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
